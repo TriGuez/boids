18910f94f820209161ec3601aead6923d42f6926
SoKatrice <68659913+SoKatrice@users.noreply.github.com>
2020-07-22 22:23:25 +0200

    allez

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from PyQt5 import QtWidgets, uic, QtCore, QtOpenGL
 import sys
 from OpenGL import GL
 from boid import *
-from ui_functions import *
 
 class Ui(QtWidgets.QMainWindow): #Classe de la fenetre d'application #
     def __init__(self):
@@ -10,6 +9,25 @@ class Ui(QtWidgets.QMainWindow): #Classe de la fenetre d'application #
         uic.loadUi('uiii.ui', self) # Load the .ui file
         self.setWindowTitle("ye booooids")
         self.show()
+
+        self.Btn_Toggle.clicked.connect(self.toggle_menu(self,250,True))
+
+        def toggle_menu(self, maxWidth, enable) : 
+        	if enable : 
+        		width = self.frame_left_menu.width()
+        		maxExtend = maxWidth
+        		standard = 70
+
+        		if width == 70 :
+        			widthExtended = maxExtend
+        		else :
+        			widthExtended = standard
+        		self.animation = QpropertyAnimation(self.frame_left_menu, b"minimumWidth")
+        		self.animation.setDuration(400)
+        		self.animation.setStartValue(width)
+        		self.animation.setEndValue(widthExtended)
+        		self.animation.start()
+
 
 app = QtWidgets.QApplication(sys.argv)
 window = Ui()
