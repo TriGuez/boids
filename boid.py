ad356cefecc9e1e2a4d3c362ff1e03e6f7020d23
Thomas LR <thomas.xuorel@gmail.com>
2020-07-23 12:10:42 +0200

    Ajout de random dans déplacement. Souci : Trop linéaire ?

--- boid.py
+++ boid.py
@@ -1,4 +1,5 @@
-from math import cos,sin
+from math import cos,sin,pi
+import random
 
 ################################
 # Classe correspondant aux boids
@@ -25,12 +26,14 @@ class boid:
         self.nouveau_x = 0
         self.nouveau_y = 0
 
+        self.chmgt_angle = 0
+
 
 
     # Boid se déplace
     def boidMove(self):
-        self.nouveau_x = self.x + self.v *cos(self.a)
-        self.nouveau_y = self.x + self.v *sin(self.a)
+        self.nouveau_x = self.x + self.v * cos(self.a)
+        self.nouveau_y = self.x + self.v * sin(self.a)
 
     # Attention gérer les autres dudez
     def boidMeet(self):
@@ -40,8 +43,30 @@ class boid:
     def boidAvoid(self):
         pass
 
+    # Mise à jour des x et y, et rajout de hasard dans l'orientation
+    def boidUpdate(self):
+        self.x = self.nouveau_x
+        self.y = self.nouveau_y
+
+        if(self.chmgt_angle == 10):
+            nouveau_a = (random.randint(0,360) * pi / 180)
+            
+            print(str(nouveau_a))
+            self.a = self.a + nouveau_a
+            #self.a = nouveau_a
+            self.chmgt_angle = 0
+        else:
+            self.chmgt_angle = self.chmgt_angle + 1
+        
+
     # Pour débug
     def boidPrint(self):
         print("x : " + str(self.x) + " & y : " + str(self.y))
         print(" New x : "+ str(self.nouveau_x) +"    New y :" + str(self.nouveau_y))
         print("------------------")
+
+    def get_x(self):
+        return self.x
+    
+    def get_y(self):
+        return self.y
--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 from math import pi
 from boid import *
+import matplotlib.pyplot as plt
 
-B1 = boid(1,1,3,(120 * pi /180))
 
-B1.boidMove()
 
-B1.boidPrint()
+B1 = boid(1,1,1,(120 * pi /180))
+B1_x = []
+B1_y = []
 
+i = 0
+while(i<100):
+    B1.boidMove()
+    B1_x.append(B1.get_x())
+    B1_y.append(B1.get_y())
+    B1.boidPrint()
+
+    B1.boidUpdate()
+
+    i = i+1
+
+plt.plot(B1_x,B1_y)
+plt.show()
