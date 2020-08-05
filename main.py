895a515e9169eeec118faa52a086d92ad3929980
SoKatrice <68659913+SoKatrice@users.noreply.github.com>
2020-08-05 18:13:29 +0200

    on repart de 1 boid

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# coding : utf-8 
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QPropertyAnimation, QThread, pyqtSignal
@@ -9,84 +8,85 @@ from random import randint
 from math import pi
 
 class Simu(QThread) :
-	new_positions=pyqtSignal(int,int)
+	new_x=pyqtSignal(int,int)
 	
 
-	def run(self) : 
-		
-		self.flock = boid(randint(0,1130),randint(0,760),1,(120 * pi /180))
+	def run(self) :
+		self.flock = boid(90,92,1,120)
 		while True :
-			self.flock.boidBehave()
-			self.new_positions.emit(self.flock.get_x(),self.flock.get_y())
-			time.sleep(.0001)
+			self.update_pos()
+
+	def update_pos(self) :
+		'''
+		x=randint(0, 1130-20)
+		y=randint(0, 760-20)
+		self.new_x.emit(x,y)
+		time.sleep(.5)
+		'''
+		self.flock.boidBehave()
+		x=self.flock.get_x()
+		y=self.flock.get_y()
+		self.new_x.emit(x,y)
+		time.sleep(.01)
 
 
+class Ui(QtWidgets.QMainWindow): #Classe de la fenetre d'application #
+    def __init__(self):
+        super(Ui, self).__init__() # Call the inherited classes __init__ method
+        uic.loadUi('uiii.ui', self) # Load the .ui file
+        self.setWindowTitle("ye booooids")
+
+        self.Btn_Toggle.clicked.connect(self.toggle_menu)
+        self.start_btn.clicked.connect(self.start_stop)
+        self.show()
+        self.scene = QGraphicsScene(self)
+        self.graph.setScene(self.scene)
+        self.scene.setSceneRect(0,0,1130,760)
+        self.simul=Simu()
 
 
+    def populate(self) :
+    	bobo=QPixmap('boid.png')
+    	self.l = QGraphicsPixmapItem(bobo)
+    	self.l.setPos(10,200)
+    	
+    	
+    	
+
+    def toggle_menu(self) : 
+        maxWidth = 250
+        enable = True
+        if enable : 
+        	width = self.frame_left_menu.width()
+        	maxExtend = maxWidth
+        	standard = 70
+
+        	if width == 70 :
+        		widthExtended = maxExtend
+        	else :
+        		widthExtended = standard
+        	self.animation = QPropertyAnimation(self.frame_left_menu, b"minimumWidth")
+        	self.animation.setDuration(400)
+        	self.animation.setStartValue(width)
+        	self.animation.setEndValue(widthExtended)
+        	self.animation.start()
+
+    def start_stop(self) :
+    	self.simul.new_x.connect(self.update_pos)
+    	if self.start_btn.text() == 'Start' :
+    		self.start_btn.setText("Stop")
+    		self.populate()
+    		self.scene.addItem(self.l)
+    		self.simul.start()
 
 
-class Ui(QtWidgets.QMainWindow):
-	def __init__(self):
-		super(Ui, self).__init__() 
-		uic.loadUi('uiii.ui', self) 
-		self.setWindowTitle("ye booooids")
+    	else :
+    		self.start_btn.setText("Start")
+    		self.simul.terminate()
+    		self.scene.clear()
 
-		self.Btn_Toggle.clicked.connect(self.toggle_menu)
-		self.start_btn.clicked.connect(self.start_stop)
-		self.show()
-		self.scene = QGraphicsScene(self)
-		self.graph.setScene(self.scene)
-		self.scene.setSceneRect(0,0,1130,760)
-		self.noboids = 1
-		self.simul=Simu()
-		self.isPopulate = False
-
-
-	def populate(self) : # si le flag is_populate est False, on remplit la scnène avec les petits cercles
-		self.boids = [QGraphicsPixmapItem(QPixmap("boid.png")) for _ in range (self.noboids)] # creéation des petits cercles correspondants à chaque boid #
-		for k in self.boids :
-			self.scene.addItem(k)
-		self.isPopulate = True
-		
-		
-
-	def toggle_menu(self) : #pour el menu dépliant
-		maxWidth = 250
-		enable = True
-		if enable : 
-			width = self.frame_left_menu.width()
-			maxExtend = maxWidth
-			standard = 70
-
-			if width == 70 :
-				widthExtended = maxExtend
-			else :
-				widthExtended = standard
-			self.animation = QPropertyAnimation(self.frame_left_menu, b"minimumWidth")
-			self.animation.setDuration(400)
-			self.animation.setStartValue(width)
-			self.animation.setEndValue(widthExtended)
-			self.animation.start()
-
-	def start_stop(self) : # on lance le thread de calcul des positions, ou alors on l'arrête suvant l'affichage du bouton Start/stop
-		self.simul.new_positions.connect(self.update_pos) # on "connecte" le signal de la liste envoyé par le thread a chaque déplacement des boids à la foncton update_pos
-		if self.start_btn.text() == 'Start' :
-			self.start_btn.setText("Stop")
-			self.simul.start()
-
-
-		else :
-			self.start_btn.setText("Start")
-			self.simul.terminate()
-			self.scene.clear()
-			self.isPopulate = False
-
-	def update_pos(self,x,y) :
-		if not self.isPopulate :
-			self.populate() #si la scene est vide, on la rempli
-		else :
-			for i in range (self.noboids) :
-				self.boids[i].setPos(x,y)
+    def update_pos(self,x,y) :
+    	self.l.setPos(x, y)
 
 
 app = QtWidgets.QApplication(sys.argv)
