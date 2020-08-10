db6eaf68816bf89ecea60d8d6a8211d230ce98f1
SoKatrice <68659913+SoKatrice@users.noreply.github.com>
2020-08-10 18:21:47 +0200

    not working yet

--- main.py
+++ main.py
@@ -12,81 +12,83 @@ class Simu(QThread) :
 	
 
 	def run(self) :
-		self.flock = boid(90,92,1,120)
+		self.flock = boid(randint(0,1130),randint(0,760),1,120)
 		while True :
-			self.update_pos()
+			x,y=self.update_pos()
+			self.new_x.emit(x,y)
+			time.sleep(.1)
 
 	def update_pos(self) :
-		'''
-		x=randint(0, 1130-20)
-		y=randint(0, 760-20)
-		self.new_x.emit(x,y)
-		time.sleep(.5)
-		'''
 		self.flock.boidBehave()
 		x=self.flock.get_x()
 		y=self.flock.get_y()
-		self.new_x.emit(x,y)
-		time.sleep(.01)
+		return x,y
+		
 
 
 class Ui(QtWidgets.QMainWindow): #Classe de la fenetre d'application #
-    def __init__(self):
-        super(Ui, self).__init__() # Call the inherited classes __init__ method
-        uic.loadUi('uiii.ui', self) # Load the .ui file
-        self.setWindowTitle("ye booooids")
+	def __init__(self):
+		super(Ui, self).__init__() # Call the inherited classes __init__ method
+		uic.loadUi('uiii.ui', self) # Load the .ui file
+		self.setWindowTitle("ye booooids")
 
-        self.Btn_Toggle.clicked.connect(self.toggle_menu)
-        self.start_btn.clicked.connect(self.start_stop)
-        self.show()
-        self.scene = QGraphicsScene(self)
-        self.graph.setScene(self.scene)
-        self.scene.setSceneRect(0,0,1130,760)
-        self.simul=Simu()
-
-
-    def populate(self) :
-    	bobo=QPixmap('boid.png')
-    	self.l = QGraphicsPixmapItem(bobo)
-    	self.l.setPos(10,200)
-    	
-    	
-    	
-
-    def toggle_menu(self) : 
-        maxWidth = 250
-        enable = True
-        if enable : 
-        	width = self.frame_left_menu.width()
-        	maxExtend = maxWidth
-        	standard = 70
-
-        	if width == 70 :
-        		widthExtended = maxExtend
-        	else :
-        		widthExtended = standard
-        	self.animation = QPropertyAnimation(self.frame_left_menu, b"minimumWidth")
-        	self.animation.setDuration(400)
-        	self.animation.setStartValue(width)
-        	self.animation.setEndValue(widthExtended)
-        	self.animation.start()
-
-    def start_stop(self) :
-    	self.simul.new_x.connect(self.update_pos)
-    	if self.start_btn.text() == 'Start' :
-    		self.start_btn.setText("Stop")
-    		self.populate()
-    		self.scene.addItem(self.l)
-    		self.simul.start()
+		self.Btn_Toggle.clicked.connect(self.toggle_menu)
+		self.start_btn.clicked.connect(self.start_stop)
+		self.show()
+		self.scene = QGraphicsScene(self)
+		self.graph.setScene(self.scene)
+		self.scene.setSceneRect(0,0,1130,760)
+		self.noboids = 2
+		self.simul=Simu()
 
 
-    	else :
-    		self.start_btn.setText("Start")
-    		self.simul.terminate()
-    		self.scene.clear()
+	def populate(self) :
+		self.circles = []
+		self.threads = []
+		for _ in range (self.noboids) :
+			self.circles.append(QGraphicsPixmapItem(QPixmap('boid.png')))
+			self.threads.append(Simu())
 
-    def update_pos(self,x,y) :
-    	self.l.setPos(x, y)
+		
+
+	def toggle_menu(self) : 
+		maxWidth = 250
+		enable = True
+		if enable : 
+			width = self.frame_left_menu.width()
+			maxExtend = maxWidth
+			standard = 70
+
+			if width == 70 :
+				widthExtended = maxExtend
+			else :
+				widthExtended = standard
+			self.animation = QPropertyAnimation(self.frame_left_menu, b"minimumWidth")
+			self.animation.setDuration(400)
+			self.animation.setStartValue(width)
+			self.animation.setEndValue(widthExtended)
+			self.animation.start()
+
+	def start_stop(self) :
+		#self.simul.new_x.connect(self.update_pos)
+		if self.start_btn.text() == 'Start' :
+			self.start_btn.setText("Stop")
+			self.populate()
+			for i in range (self.noboids) :
+				self.scene.addItem(self.circles[i])
+				self.threads[i].new_x.connect(self.update_pos)
+				self.threads[i].start()
+
+
+		else :
+			self.start_btn.setText("Start")
+			self.scene.clear()
+			for i in range (self.noboids) :
+				self.threads[i].terminate()
+
+	def update_pos(self,x,y) :
+		for i in range (self.noboids) :
+			self.circles[i].setPos(x,y)
 
 
 app = QtWidgets.QApplication(sys.argv)
