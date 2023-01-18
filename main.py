a9bec003ff20b28cb1023aa24131aeb9166ab018
TriGuez <121152666+TriGuez@users.noreply.github.com>
2023-01-18 08:33:45 +0100

    remove old functions

--- main.py
+++ main.py
@@ -24,27 +24,9 @@ class Simu(QThread) :
 		x = np.asarray([randint(0,1130) for _ in range(self.noBoids)])
 		y = np.asarray([randint(0,760) for _ in range(self.noBoids)])
 		while True :
-			#for ji in range(self.noBoids) :
-			#	x[ji],y[ji], angle[ji]=UpdateBoidPos(x[ji], y[ji], angle[ji], self.speed, self.distance, self.cohes)
 			x, y, angle = UpdateBoidPos(x, y, angle, self.speed, self.distance, self.cohes)
 			self.new_x.emit(x,y)
 			time.sleep(.01)
-
-	def update_pos(self, x, y, angle, speed) :
-		x+= speed*sqrt(2) * cos(angle)
-		y+= speed*sqrt(2) * sin(angle)
-		if x >= 1130 :
-			angle = (pi-angle)
-		if x <= 0 :
-			angle = pi-angle
-		if y >= 760 :
-			angle = (-angle)
-		if y <= 0 :
-			angle = -angle
-		return x,y, angle
-	def UpdateSpeed(self,val) :
-		self.testspeed = val
-		self.FlagChangedSlider = True
 
 
 class Ui(QtWidgets.QMainWindow): #Classe de la fenetre d'application #
